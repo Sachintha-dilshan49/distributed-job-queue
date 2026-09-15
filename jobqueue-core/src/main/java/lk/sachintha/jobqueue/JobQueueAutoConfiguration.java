e1eb03116c3de05f33be8477e4a10a5c996fb5a4
Sachintha <sachintharcm@gmail.com>
2026-09-15 16:48:54 +0530

    Phase 3: auto-configuration for jobqueue-core

---
+++ jobqueue-core/src/main/java/lk/sachintha/jobqueue/JobQueueAutoConfiguration.java
@@ -0,0 +1,9 @@
+package lk.sachintha.jobqueue;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan(basePackages = "lk.sachintha.jobqueue")
+public class JobQueueAutoConfiguration {
+}
